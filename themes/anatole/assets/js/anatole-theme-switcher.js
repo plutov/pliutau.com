16af06d1e765a840687ef1322d5a8f249b0b479b
Alex Pliutau <a.pliutau@gmail.com>
2024-08-16 11:10:56 +0200

    dark mode, no substack banner

--- themes/anatole/assets/js/anatole-theme-switcher.js
+++ themes/anatole/assets/js/anatole-theme-switcher.js
@@ -1,47 +1,50 @@
 // initialize default value
 function getTheme() {
-    return localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+	return localStorage.getItem("theme") ? localStorage.getItem("theme") : null;
 }
 
 function setTheme(style) {
-    document.documentElement.setAttribute('data-theme', style);
-    localStorage.setItem('theme', style);
+	document.documentElement.setAttribute("data-theme", style);
+	localStorage.setItem("theme", style);
 }
 
 function init() {
-    // initialize default value
-    const theme = getTheme();
+	// initialize default value
+	const theme = getTheme();
 
-    if (theme === null) {
-        if (!document.documentElement.getAttribute('data-theme')) {
-            setTheme('light');
-        } else {
-            setTheme(document.documentElement.getAttribute('data-theme'));
-        }
-    } else {
-        // load a stored theme
-        if (theme === 'light') {
-            document.documentElement.setAttribute('data-theme', 'light');
-        } else {
-            document.documentElement.setAttribute('data-theme', 'dark');
-        }
-    }
+	if (theme === null) {
+		if (!document.documentElement.getAttribute("data-theme")) {
+			setTheme("dark");
+		} else {
+			setTheme(document.documentElement.getAttribute("data-theme"));
+		}
+	} else {
+		// load a stored theme
+		if (theme === "light") {
+			document.documentElement.setAttribute("data-theme", "light");
+		} else {
+			document.documentElement.setAttribute("data-theme", "dark");
+		}
+	}
 }
-
 
 // switch themes
 function switchTheme() {
-    const theme = getTheme();
-    if (theme === 'light') {
-        setTheme('dark');
-    } else {
-        setTheme('light');
-    }
+	const theme = getTheme();
+	if (theme === "light") {
+		setTheme("dark");
+	} else {
+		setTheme("light");
+	}
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const themeSwitcher = document.querySelector('.theme-switch');
-    themeSwitcher.addEventListener('click', switchTheme, false);
-}, false);
+document.addEventListener(
+	"DOMContentLoaded",
+	function () {
+		const themeSwitcher = document.querySelector(".theme-switch");
+		themeSwitcher.addEventListener("click", switchTheme, false);
+	},
+	false,
+);
 
 init();
